27e2a0273700ed0060333587fa3b4bf20381091e
alexandrtovmach <alexandrtovmach@gmail.com>
2025-08-14 22:31:17 +0200

    feat: Added OHM as experiment

--- components/MapField/index.tsx
+++ components/MapField/index.tsx
@@ -4,4 +4,9 @@ const MapField = dynamic(() => import("./Input").then((mod) => mod.default), {
   ssr: false,
 });
 
+export const HistoricalMap = dynamic(
+  () => import("./HistoricalMap").then((mod) => mod.default),
+  { ssr: false },
+);
+
 export default MapField;
