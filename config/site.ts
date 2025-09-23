138cf9e0480d7c7485f42327f70a87df42e58ab5
alexandrtovmach <alexandrtovmach@gmail.com>
2025-09-23 10:37:23 +0200

    fix: Title change

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
+  name: "Редактор",
   description: "Make beautiful websites regardless of your design experience.",
   url: "https://editor.duckarchive.com",
 };
